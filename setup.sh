1c47c96e820682d0fcf8f9a8d02d78f470d0906a
bh10-d <duchieubui25012002@gmail>
2024-08-29 10:28:37 +0700

    logic check ansible.cfg and ansible exists

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 GET_PWD="$(pwd)"
+WOR_ANSIBLE="/etc/ansible"
 
 sudo apt update
 
@@ -27,9 +28,9 @@ sudo cp /usr/local/share/kolla-ansible/ansible/inventory/* /etc/kolla/
 kolla-ansible install-deps
 
 sleep 50
-echo -e "\033[1;34m ======================================================================"
-echo -e "\033[1;34m Create ansible folder and create ansible.cfg file"
-echo -e "\033[1;34m ======================================================================"
+echo -e "\033[1;34m========================================================================"
+echo -e "\033[1;34mCreate ansible folder and create ansible.cfg file"
+echo -e "\033[1;34m========================================================================"
 
 sudo mkdir /etc/ansible
 sudo touch /etc/ansible/ansible.cfg
@@ -39,22 +40,52 @@ pipelining=True
 forks=100" | sudo tee /etc/ansible/ansible.cfg > /dev/null
 
 
+if [ -d "$WOR_ANSIBLE" ]; then
+  ### Take action if $DIR exists ###
+  FILE=/etc/ansible/ansible.cfg
+  if [ -f "$FILE" ]; then
+    echo -e "\033[1;33m$FILE exists."
+    echo "[defaults]
+host_key_checking=False
+pipelining=True
+forks=100" | sudo tee /etc/ansible/ansible.cfg > /dev/null
+  else 
+    #echo "$FILE does not exist."
+    sudo touch /etc/ansible/ansible.cfg
+    echo "[defaults]
+host_key_checking=False
+pipelining=True
+forks=100" | sudo tee /etc/ansible/ansible.cfg > /dev/null
+  fi
+else
+  ###  Control will jump here if $DIR does NOT exists ###
+  echo "Create folder /etc/ansible"
+  sudo mkdir /etc/ansible
+  sudo touch /etc/ansible/ansible.cfg
+  echo "[defaults]
+host_key_checking=False
+pipelining=True
+forks=100" | sudo tee /etc/ansible/ansible.cfg > /dev/null
+  exit 1
+fi
+
+
 sleep 50
-echo -e "\033[1;34m ======================================================================"
-echo -e "\033[1;34m Running command kolla-genpwd"
-echo -e "\033[1;34m ======================================================================"
+echo -e "\033[1;34m========================================================================"
+echo -e "\033[1;34mRunning command kolla-genpwd"
+echo -e "\033[1;34m========================================================================"
 kolla-genpwd
 
 sleep 50
-echo -e "\033[1;34m ======================================================================"
-echo -e "\033[1;34m Config globals.yml file in /etc/kolla"
-echo -e "\033[1;34m ======================================================================"
+echo -e "\033[1;34m========================================================================"
+echo -e "\033[1;34mConfig globals.yml file in /etc/kolla"
+echo -e "\033[1;34m========================================================================"
 cat $GET_PWD/sglobals.conf | sudo tee -a /etc/kolla/globals.yml > /dev/null
 
 sleep 50
-echo -e "\033[1;34m ========================================================================"
-echo -e "\033[1;34m ======== Bootstrap-servers ======== Prechecks ========= Deploy ========="
-echo -e "\033[1;34m ========================================================================"
+echo -e "\033[1;34m========================================================================"
+echo -e "\033[1;34m======== Bootstrap-servers ======== Prechecks ========= Deploy ========="
+echo -e "\033[1;34m========================================================================"
 cd /etc/kolla
 kolla-ansible -i ./all-in-one bootstrap-servers &
 wait
@@ -64,14 +95,14 @@ kolla-ansible -i ./all-in-one deploy &
 wait
 
 sleep 50
-echo -e "\033[1;34m ======================================================================"
-echo -e "\033[1;34m Download Openstack"
-echo -e "\033[1;34m ======================================================================"
+echo -e "\033[1;34m========================================================================"
+echo -e "\033[1;34mDownload Openstack"
+echo -e "\033[1;34m========================================================================"
 pip3 install python-openstackclient -c https://releases.openstack.org/constraints/upper/master
 
-echo -e "\033[1;34m ======================================================================"
-echo -e "\033[1;34m Deploy Openstack"
-echo -e "\033[1;34m ======================================================================"
+echo -e "\033[1;34m========================================================================"
+echo -e "\033[1;34mDeploy Openstack"
+echo -e "\033[1;34m========================================================================"
 kolla-ansible post-deploy
 
 cat /etc/kolla/admin-openrc.sh
